0b6fbda55e7700ff4d46c42185fc15ee160f772a
pravallika206 <naragonipravallika2005@gmail.com>
2024-11-12 10:39:28 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class java
+public static void main(String[] args)
+{
+Scanner SC = new Scanner(System.in);
+int X = SC.nextInt();
+int Y = SC.nextInt();
+if (X > Y)
+{
+System.out.println(X);
+} else { 
+System.out.println(Y);
+}
+}

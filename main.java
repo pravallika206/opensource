4ef26c147e641e9592c5768a19226767b009827b
pravallika206 <naragonipravallika2005@gmail.com>
2024-11-12 10:50:02 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner SC = new Scanner(System.in);
+        int N = SC.nextInt();
+        int numberofNotesBooks = N * 10;
+        System.out.println(numberofNotesBooks);
+    
+    }
+}
